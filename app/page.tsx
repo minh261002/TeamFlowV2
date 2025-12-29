607a81c15664327c9ac280d5a437fa77418cb59a
Minh Tran <trcongminh.261002@gmail.com>
2025-12-29 20:59:58 +0700

    feat: implement user authentication in TRPC context and protect getUsers query

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
   await requireAuth();
-  return <div></div>;
+  const data = await caller.getUsers();
+  return <div>{JSON.stringify(data, null)}</div>;
 };
 
 export default Page;
