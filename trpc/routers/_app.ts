25af81b7378d963ffb6d2cb68dfc9c580fce2d7b
Minh Tran <trcongminh.261002@gmail.com>
2025-12-30 11:06:50 +0700

    feat: Add a dedicated workflows feature, including a new tRPC router for CRUD operations and a client-side hook for data fetching.

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,31 +1,8 @@
-import { inngest } from "@/inngest/client";
-import {
-  createTRPCRouter,
-  premiumProcedure,
-  protectedProcedure,
-} from "../init";
-import prisma from "@/lib/db";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testGoogleAi: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-      data: {},
-    });
-
-    return { success: true, message: "AI job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: { email: "minh@gmail.com" },
-    });
-
-    return { success: true, message: "Job queued" };
-  }),
+  workflows: workflowsRouter,
 });
 
 export type AppRouter = typeof appRouter;
